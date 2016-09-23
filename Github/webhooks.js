6750297284873fd1c712f1c181f79189b47f35cf
David Sevilla Martin <dsevilla192@icloud.com>
2016-09-23 17:17:27 -0400

    Added validate secret func

--- Github/webhooks.js
+++ Github/webhooks.js
@@ -1,12 +1,17 @@
 const GithubEvents = require('./GithubEvents');
 const Log = require('../lib/Logger').Logger;
 
+const ValidateSecret = secret => {
+  let signature = 'sha1=' + crypto.createHmac('sha256', process.env.GITHUB_SECRET).digest('hex');
+  return signature == secret;
+}
+
 module.exports = (req, res, next) => {
   const event = req.headers['x-github-event'];
   const secret = req.headers['x-hub-signature'];
   const data = req.body;
 
-  if (!secret || secret !== process.env.GITHUB_SECRET) return res.status(401).json({ error: `${secret} is an invalid secret` });
+  if (!secret || !ValidateSecret(secret)) return res.status(401).json({ error: `${secret} is an invalid secret` });
 
   switch (event) {
     case 'push': {

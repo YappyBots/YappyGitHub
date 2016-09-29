bbccfb17ad1167c5a7a7105b7c1fa5dc1a0ff828
David Sevilla Martin <dsevilla192@icloud.com>
2016-09-29 19:41:11 -0400

    OOPS! Anyone could use `T! exec` (facepalm)

--- commands/Exec.js
+++ commands/Exec.js
@@ -1,6 +1,7 @@
 const { spawn, exec } = require('child_process');
 const now = require('performance-now');
 const Log = require('../lib/Logger').Logger;
+const Owner = '175008284263186437';
 
 const Exec = (cmd, opts = {}) => {
   let startTime = now();
@@ -23,60 +24,59 @@ const clean = text => {
   }
 }
 
-module.exports = bot => {
-  return (msg, command) => {
+module.exports = bot => (msg, command) => {
+  if (msg.author.id !== Owner) return false;
 
-    let runningMessage = [
-      '`RUNNING`',
+  let runningMessage = [
+    '`RUNNING`',
+    '```xl',
+    clean(command),
+    '```'
+  ].join('\n')
+
+  let messageToEdit;
+
+  msg.channel.sendMessage(runningMessage).then(message => {
+    messageToEdit = message;
+  }).then(() => Exec(command))
+  .then(data => {
+    let { stdout, startTime, endTime } = data;
+
+    stdout = stdout.substring(0, stdout.length - 1);
+
+    let message = [
+      '`EXEC`',
       '```xl',
       clean(command),
+      '```',
+      '`STDOUT`',
+      '```xl',
+      clean(stdout),
       '```'
-    ].join('\n')
+    ].join('\n');
 
-    let messageToEdit;
+    messageToEdit.edit(message);
+  }).catch(data => {
+    if (data && data.stack) {
+      Log.error(data);
+      throw data;
+    }
+    let { stderr, startTime, endTime } = data;
 
-    msg.channel.sendMessage(runningMessage).then(message => {
-      messageToEdit = message;
-    }).then(() => Exec(command))
-    .then(data => {
-      let { stdout, startTime, endTime } = data;
+    stderr = stderr.substring(0, stderr.length - 1);
 
-      stdout = stdout.substring(0, stdout.length - 1);
+    let message = [
+      '`EXEC`',
+      '```xl',
+      clean(command),
+      '```',
+      '`STDERR`',
+      '```xl',
+      clean(stderr),
+      '```'
+    ].join('\n');
 
-      let message = [
-        '`EXEC`',
-        '```xl',
-        clean(command),
-        '```',
-        '`STDOUT`',
-        '```xl',
-        clean(stdout),
-        '```'
-      ].join('\n');
+    messageToEdit.edit(message);
+  });
 
-      messageToEdit.edit(message);
-    }).catch(data => {
-      if (data && data.stack) {
-        Log.error(data);
-        throw data;
-      }
-      let { stderr, startTime, endTime } = data;
-
-      stderr = stderr.substring(0, stderr.length - 1);
-
-      let message = [
-        '`EXEC`',
-        '```xl',
-        clean(command),
-        '```',
-        '`STDERR`',
-        '```xl',
-        clean(stderr),
-        '```'
-      ].join('\n');
-
-      messageToEdit.edit(message);
-    });
-
-  }  
 }

6e0a7aa7a80823455decd64748b851b4255f1993
David Sevilla Martin <dsevilla192@icloud.com>
2016-10-08 16:00:19 -0400

    bolded and changed var in gollum event

--- Github/Gollum.js
+++ Github/Gollum.js
@@ -3,7 +3,7 @@ module.exports = (data) => {
   let pages = data.pages;
   let actions = pages.map(e => {
     let action = e.action == 'edited' ? 'Edited' : 'Created';
-    return `${e.action} _${e.title}_ (<${e.html_url}>)`;
+    return `${action} **${e.title}** (<${e.html_url}>)`;
   }).join('\n          ')
 
   let msg = `📰 **${actor.login}** modified the wiki\n          ${actions}`;

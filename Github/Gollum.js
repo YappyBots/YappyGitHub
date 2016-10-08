01b6ac64dd9e7d008082cf741c4c022cdd96f25a
David Sevilla Martin <dsevilla192@icloud.com>
2016-10-08 15:59:13 -0400

    Whoops wiki error, title is not defined

--- Github/Gollum.js
+++ Github/Gollum.js
@@ -3,7 +3,7 @@ module.exports = (data) => {
   let pages = data.pages;
   let actions = pages.map(e => {
     let action = e.action == 'edited' ? 'Edited' : 'Created';
-    return `${e.action} _${title}_ (<${e.html_url}>)`;
+    return `${e.action} _${e.title}_ (<${e.html_url}>)`;
   }).join('\n          ')
 
   let msg = `📰 **${actor.login}** modified the wiki\n          ${actions}`;

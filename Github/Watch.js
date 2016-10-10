bb4ef8eed4498dac14f8874bd409a1a5acdaeb56
David Sevilla Martin <dsevilla192@icloud.com>
2016-10-10 17:36:35 -0400

    changed emoji for watch (star) to a star

--- Github/Watch.js
+++ Github/Watch.js
@@ -1,4 +1,4 @@
 module.exports = payload => {
   let starred = payload.action == 'started';
-  return `👀 **${payload.sender.login}** ${starred ? 'starred' : 'unstarred'} ${payload.repository.full_name}`;
+  return `⭐ **${payload.sender.login}** ${starred ? 'starred' : 'unstarred'} ${payload.repository.full_name}`;
 }

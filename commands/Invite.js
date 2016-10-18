69e45ca06683b1f02a25b31b9dcbc14fe5887485
David Sevilla Martin <dsevilla192@icloud.com>
2016-10-17 20:08:07 -0400

    Don't embed invite link as a normal link in invite command

--- commands/Invite.js
+++ commands/Invite.js
@@ -11,7 +11,7 @@ const message = [
   '  2. Run `G! init user/repo` in the channel where you want to get the events',
   '',
   '__Official Server__',
-  '**http://discord.gg/HHqndMG**'
+  '**<http://discord.gg/HHqndMG>**'
 ];
 
 class InviteCommand extends Command {

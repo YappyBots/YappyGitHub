4e877d0b681b57054861542fb9f4c3b53de867e8
David Sevilla Martin <dsevilla192@icloud.com>
2016-10-29 14:14:16 -0400

    Fixed stats command

--- commands/Stats.js
+++ commands/Stats.js
@@ -1,6 +1,5 @@
 const Command = require('../lib/Structures/Command');
 const Log = require('../lib/Logger').Logger;
-const BotCache = require('../lib/BotCache');
 const moment = require('moment');
 const pack = require('../package.json');
 const DiscordJS = require('discord.js');
@@ -8,67 +7,43 @@ const DiscordJS = require('discord.js');
 require('moment-duration-format');
 
 const unit = ['', 'K', 'M', 'G', 'T', 'P'];
-const GetUptime = bot => {
-  return moment.duration(bot.uptime).format('d[ days], h[ hours], m[ minutes, and ]s[ seconds]');
-};
+const GetUptime = bot => moment.duration(bot.uptime).format('d[ days], h[ hours], m[ minutes, and ]s[ seconds]');
 const bytesToSize = (input, precision) => {
-	let index = Math.floor(Math.log(input) / Math.log(1024));
-	if (unit >= unit.length) return input + ' B';
-	return (input / Math.pow(1024, index)).toFixed(precision) + ' ' + unit[index] + 'B';
-}
+  let index = Math.floor(Math.log(input) / Math.log(1024));
+  if (unit >= unit.length) return `${input} B`;
+  let msg = `${(input / Math.pow(1024, index)).toFixed(precision)} ${unit[index]}B`;
+  return msg;
+};
 
 class StatsCommand extends Command {
   constructor(bot) {
     super(bot);
-
     this.props.help = {
       name: 'stats',
       description: 'Shows some stats of the bot... what else?',
-      usage: 'stats'
-    }
-
+      usage: 'stats',
+    };
     this.setConf({
-      aliases: ['info']
+      aliases: ['info'],
     });
   }
-
   run(msg) {
     let bot = this.bot;
-
     let MemoryUsage = bytesToSize(process.memoryUsage().rss, 3);
-    let MessagesSeen = BotCache._MessagesSeen.size;
-    let MessagesSent = BotCache._MessagesSent.size + 1;
     let Booted = bot.booted;
-
     let TextChannels = bot.channels.filter(e => e.type !== 'voice').size;
     let VoiceChannels = bot.channels.filter(e => e.type === 'voice').size;
-
     let Dependencies = new Map();
-    Dependencies.set('moment', pack.dependencies['moment'].split('^')[1]);
-    Dependencies.set('mongoose', pack.dependencies['mongoose'].split('^')[1]);
-    Dependencies.set('socket.io', pack.dependencies['socket.io'].split('^')[1]);
-    Dependencies.set('chalk', pack.dependencies['chalk'].split('^')[1]);
-    Dependencies.set('express', pack.dependencies['express'].split('^')[1]);
-
+    Dependencies.set('moment', pack.dependencies.moment.split('^')[1]);
     let dependencies = [
-      [
-        `Moment v${Dependencies.get('moment')}`,
-        `Mongoose v${Dependencies.get('mongoose')}`,
-        `Socket.IO v${Dependencies.get('socket.io')}`
-      ].join(', '),
-      [
-        `Chalk v${Dependencies.get('chalk')}`,
-        `ExpressJS v${Dependencies.get('express')}`
-      ].join(', ')
-    ].join('\n' + ' '.repeat(17))
+      `Moment v${Dependencies.get('moment')}`,
+    ].join(`\n${' '.repeat(17)}`);
 
     let message = [
       `# STATISTICS`,
       `Uptime         : ${GetUptime(bot)}`,
       `Booted         : ${Booted.date} @ ${Booted.time}`,
       `Memory Usage   : ${MemoryUsage}`,
-      `Messages Seen  : ${MessagesSeen}`,
-      `Messages Sent  : ${MessagesSent}`,
       ``,
       `# SERVING`,
       `Guilds         : ${bot.guilds.size}`,
@@ -77,12 +52,12 @@ class StatsCommand extends Command {
       ``,
       `# BOT INFORMATION`,
       `Author(s)      : ${pack.author.replace(/<\S+[@]\S+[.]\S+>/g, '')}`,
-      `Contributor(s) : ${pack.contributors || 'None'}`,
+      `Contributor(s) : ${pack.contributors ? pack.contributors.join(', ').replace(/<\S+[@]\S+[.]\S+>/g, '') : 'None'}`,
       `Discord.JS	 : v${DiscordJS.version}`,
-      `Dependencies   : ${dependencies}`
+      `Dependencies   : ${dependencies}`,
     ];
 
-    return msg.channel.sendCode('LDIF', message);
+    msg.channel.sendCode('LDIF', message).catch(Log.error);
   }
 }
 

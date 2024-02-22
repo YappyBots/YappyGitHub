8d55f3a3a3fcaf8349ace372f3487dd97ec6c6a2
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2024-02-22 17:41:53 -0500

    db: migrations > fix ignore_unknown default causing foreign key issue
    See more at knex/knex issue 5367

--- db/migrations/2023_05_28_000000_make_ignore_unknown_default.js
+++ db/migrations/2023_05_28_000000_make_ignore_unknown_default.js
@@ -1,13 +1,16 @@
 // Too many unknown events now, the default should be to ignore them
 exports.up = async (knex) =>
-  knex.schema
-    .table('channels', (t) => {
-      t.boolean('ignore_unknown').defaultTo(true).alter();
-    })
-    .then(() => knex('channels').update('ignore_unknown', true));
+  knex.transaction(async (trx) => {
+    await trx.raw(
+      'ALTER TABLE channels ADD COLUMN ignore_unknown_ DEFAULT true'
+    );
+    await trx.raw('ALTER TABLE channels DROP COLUMN ignore_unknown');
+    await trx.raw(
+      'ALTER TABLE channels RENAME COLUMN ignore_unknown_ TO ignore_unknown'
+    );
+  });
 
 // This doesn't rollback the values because it's not possible to know what they were before
-exports.down = async (knex) =>
-  knex.schema.table('channels', (t) => {
-    t.boolean('ignore_unknown').defaultTo(false).alter();
-  });
+exports.down = async (knex) => {
+  // This is a no-op
+};
